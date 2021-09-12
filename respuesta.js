c25b265bc38681c0b4e5943cf177897145b3a267
John Jairo Rodriguez Medina <johnjairorodriguez384@gmail.com>
2021-09-11 22:29:05 -0500

    final parcial portafolio - diseño responsivo

---
+++ respuesta.js
@@ -0,0 +1,9 @@
+var almacenadora = document.getElementById("texto")
+var botonn = document.getElementById("boton")
+var xx = boton.addEventListener("click" ,funcion1)
+
+function funcion1(){
+    if(almacenadora.value){
+        alert("tu información ha sido enviada")
+    }
+}
